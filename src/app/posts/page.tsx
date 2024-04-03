51bb11c4eb36ca4a06f466189f0252cc34d7deed
Ricardo Pimienta <pimientaglez@gmail.com>
2024-04-03 17:53:13 -0600

    fetch list of posts and display them

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,11 +1,54 @@
-import Link from 'next/link';
+"use client"
+import {useState, useEffect} from "react";
+import {useRouter} from "next/navigation";
+import Link from "next/link";
 
-export default function Posts() {
+export type Post = {
+  id: string;
+  content: string;
+  timestamp: string,
+  likes: number,
+};
+
+export default function Posts({params}: {params: {id: string}}) {
+    const [posts, setPosts] = useState<Post[] | null>(null); 
+    const getPost = async () => {
+        try {
+            const response = await fetch(`http://localhost:3000/api/posts/`, {method: 'GET'});
+            if(response) {
+                const result = await response.json();
+                if(result) setPosts(result);
+            }
+        } catch(e) {
+            console.error('Error => ',e);
+        }
+    }
+
+    useEffect(() => {
+      getPost();
+    }, [])
+
+    const router = useRouter();
+    const handleClick = (id: string) => {
+        router.push(`posts/${id}`);
+    }
+    
+    
     return (
-        <>
-            <Link href="/"><h3>Home</h3></Link>
-            <h1>Posts</h1>
-            <Link href={`/posts/${1}`}>Show Post Detail 1</Link>
-        </>
+        <main>
+            {(posts && posts.length > 0) && (
+                posts.map((post) => {
+                    return (
+                        <div key={post?.id} onClick={()=>{handleClick(post.id)}}>
+                            <h2>{post?.content}</h2>
+                            <h2>{post?.timestamp}</h2>
+                            <h2>{post?.likes}</h2>
+                        </div> 
+                    )
+                })
+            )
+            }
+            <Link href="/" className="text-blue-500 mr-4"><h3>Go Back</h3></Link>
+        </main>
     )
-  }
+}
