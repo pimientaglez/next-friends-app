b9105a7d1027822926b0625040eaf754e237b3bb
Ricardo Pimienta <pimientaglez@gmail.com>
2024-04-03 17:52:39 -0600

    fetch and display single post

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,12 +1,43 @@
+"use client"
+import {useState, useEffect} from "react";
 import Link from 'next/link';
 
-export default function PostId({params}: {
-    params: {id: string}
-}) {
+export type Post = {
+  id: string;
+  content: string;
+  timestamp: string,
+  likes: number,
+};
+
+export default function PostID({params}: {params: {id: string}}) {
+    const [post, setPost] = useState<Post | null>(null); 
+    const getPost = async () => {
+        try {
+            const response = await fetch(`http://localhost:3000/api/posts/${params.id}`, {method: 'GET'});
+            if(response) {
+                const result = await response.json();
+                if(result) setPost(result);
+            }
+        } catch(e) {
+            console.error('Error => ',e);
+        }
+    }
+
+    useEffect(() => {
+      getPost();
+    }, [])
+    
+    
     return (
-      <>
-        <Link href="/posts"><h3>Back to Posts</h3></Link>
-        <h1>Posts {params.id}</h1>
-      </>
+        <main>
+            {post && 
+                <>
+                    <h2>{post?.content}</h2>
+                    <h2>{post?.timestamp}</h2>
+                    <h2>{post?.likes}</h2>
+                </>
+            }
+            <Link href="/posts" className="text-blue-500 mr-4"><h3>Go Back</h3></Link>
+        </main>
     )
 }
