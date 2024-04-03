95c2701094e89ab0e83a258fd0c89899124d6a0a
Ricardo Pimienta <pimientaglez@gmail.com>
2024-04-03 16:08:28 -0600

    add mock data for user

---
+++ src/app/api/users/data.ts
@@ -0,0 +1,15 @@
+export const users = [
+    {
+        id: 1,
+        username: "rpimienta",
+        first_name: "Ricardo",
+        last_name: "Pimienta",
+        email: "rpimienta@gmail.com",
+        role: "Software Engineer",
+        location: {
+            city: "Guadalajara",
+            country: "Mexico"
+        },
+        bio: "Software engineer from México",
+    },
+];
