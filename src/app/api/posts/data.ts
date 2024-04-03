5dc67c7b2cbcd599ff7daa175f7020f584d92d5c
Ricardo Pimienta <pimientaglez@gmail.com>
2024-04-03 16:39:27 -0600

    add mock data for posts

---
+++ src/app/api/posts/data.ts
@@ -0,0 +1,32 @@
+export const posts = [
+    {
+        id: "1",
+        content: "I'm excited to share my new project. 🚀",
+        timestamp: "2024-03-25T09:00:00Z",
+        likes: 50,
+    },
+    {
+        id: "2",
+        content: "I'm excited to share my new project. 🚀",
+        timestamp: "2024-03-25T09:00:00Z",
+        likes: 100,
+    },
+    {
+        id: "3",
+        content: "I'm excited to share my new project. 🚀",
+        timestamp: "2024-03-25T09:00:00Z",
+        likes: 3,
+    },
+    {
+        id: "4",
+        content: "I'm excited to share my new project. 🚀",
+        timestamp: "2024-03-25T09:00:00Z",
+        likes: 4,
+    },
+    {
+        id: "5",
+        content: "I'm excited to share my new project. 🚀",
+        timestamp: "2024-03-25T09:00:00Z",
+        likes: 30,
+    },
+];
