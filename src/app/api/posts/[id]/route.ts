d53f0856b0a08cb4f89a732cce6ea928192b303e
Ricardo Pimienta <pimientaglez@gmail.com>
2024-09-09 18:35:56 -0600

    test lint

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import {posts} from "../data";
 
 export async function GET(_request: Request, {params}: {params: {id: string} }) {
+    const testingLint = 'my test';
     const post = posts.find((post) => {
         return post.id === parseInt(params.id)
     })
